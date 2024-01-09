2185e128be33fbf3f139712d3d138a8f2c13db06
takeshiho0531 <kawadatakeshiho@gmail.com>
2024-01-09 22:05:38 +0900

    fix: bit reversal count io

--- bit_reversal_count/bit_reversal_count.v
+++ bit_reversal_count/bit_reversal_count.v
@@ -1,19 +1,9 @@
 `timescale	1ns/1ns
 module bit_reversal_count #(
-    parameter I_BW = 14,
-    parameter O_BW = 14,
     parameter GROUP_LEN = 1024
 )(
-    input clk,
-    input rst,
-    input di_en,
-    input signed [I_BW-1:0] data_i,
-    input [6:0] in_group_num, // 0-88
     input [9:0] in_group_idx, // 0-1023
-    output reg do_en,
-    output reg signed [O_BW-1:0] data_o,
-    output reg [6:0] out_group_num,
-    output reg [9:0] out_group_idx // 0-1024
+    output [9:0] out_group_idx // 0-1024
 );
 
     wire [9:0] bit_reversal_count;
@@ -29,20 +19,6 @@ module bit_reversal_count #(
     assign bit_reversal_count[8] = in_group_idx[1];
     assign bit_reversal_count[9] = in_group_idx[0];
 
+    assign out_group_idx = bit_reversal_count;
 
-    always @(posedge clk or negedge rst) begin
-        // $display("in_group_idx=%b, bit_reversal_count=%b", in_group_idx, bit_reversal_count);
-        if (!rst) begin
-            do_en <= 0;
-            data_o <= 0;
-            out_group_idx <= 0;
-            out_group_num <= 0;
-        end
-        else begin
-            do_en <= di_en;
-            data_o <= data_i;
-            out_group_num <= in_group_num;
-            out_group_idx <= bit_reversal_count;
-        end
-    end
 endmodule
